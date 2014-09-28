1a8487518b689bdf3eca0ad6b698386f13371f44
Kushal Pradhan <pkushal@uab.edu>
2014-09-28 16:19:41 -0500

    Dynamic Titles

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+#Return a title on a per page
+	 def title
+	 	base_title ="RoR Sample"
+	 	if @title.nil?
+	 		base_title
+	 	else
+	 		"Ruby On Rails | #{@title}"
+	 	end
+	 end
 end
